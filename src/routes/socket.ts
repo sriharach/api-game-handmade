9b81c543fbc3de3bd83ff3997cdabc8e314aa0cf
folk.sriharach <folk.sriharach@bitkub.com>
2023-04-26 12:14:52 +0700

    config vercal

--- src/routes/socket.ts
+++ src/routes/socket.ts
@@ -1,20 +1,20 @@
-import express from 'express'
-import io from '../socket.io'
+// import express from 'express'
+// import io from '../socket.io'
 
-const router = express.Router()
+// const router = express.Router()
 
-router.get('/', (_req, res) => {
-  io.on('connection', (socket) => {
-    console.log(`Client ${socket.id} connected__`)
+// router.get('/', (_req, res) => {
+//   io.on('connection', (socket) => {
+//     console.log(`Client ${socket.id} connected__`)
 
-    socket.join("ROOM1");
+//     socket.join("ROOM1");
 
-    io.to("ROOM1").emit("ggo");
+//     io.to("ROOM1").emit("ggo");
     
-    socket.on('disconnect', () => {
-      console.log('user disconnected')
-    })
-  })
-})
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected')
+//     })
+//   })
+// })
 
-export default router
+// export default router
